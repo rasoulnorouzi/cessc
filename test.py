7383e7655bef2b2aa3b91d1400bf4935b9632e85
rasoulnorouzi <rasoulnorouzi@live.com>
2024-04-15 20:38:56 +0200

    adding reproducibility!

--- test.py
+++
@@ -1,8 +0,0 @@
-# %%
-from scripts.model_factory import initialize_model
-from datasets import load_dataset
-from scripts.configurations import config
-from scripts.finetuner import FineTuner
-from scripts.evaluation import evaluate
-from scripts.utility import error_analysis, set_all_seeds
-# %%
